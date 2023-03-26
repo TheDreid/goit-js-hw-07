4cc10a8174bce10e1eb8b959119d23433243848f
TheDreid <111651739+TheDreid@users.noreply.github.com>
2023-03-26 22:06:23 +0200

    26.03. upd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryItemsRef = galleryItems;
+const markup = createMarkup(galleryItemsRef);
+
+galleryRef.innerHTML = markup;
+
+function createMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="large-image.jpg">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+         </a>
+        </li>
+      `;
+    })
+    .join("");
+}
+
+galleryRef.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const selectedImgSrc = e.target.dataset.source;
+  const selectedImgAlt = e.target.alt;
+  showModal(selectedImgSrc, selectedImgAlt);
+});
+
+function showModal(imageSrc, imageAlt) {
+  const modal = basicLightbox.create(`
+    <img src="${imageSrc}" alt="${imageAlt}" width="800" height="600">
+  `);
+  modal.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryItemsRef = galleryItems;
+const markup = createMarkup(galleryItemsRef);
+
+galleryRef.innerHTML = markup;
+
+function createMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+      `;
+    })
+    .join("");
+}
+
+galleryRef.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const selectedImgSrc = e.target.dataset.source;
+  const selectedImgAlt = e.target.alt;
+  showModal(selectedImgSrc, selectedImgAlt);
+});
+
+function showModal(imageSrc, imageAlt) {
+  const modal = basicLightbox.create(`
+    <img src="${imageSrc}" alt="${imageAlt}" width="800" height="600">
+  `);
+  modal.show();
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {});
